b1964c462fce3163fc2f57b8644e69fabc2c314e
ThereWolf <bdunham484@gmail.com>
2022-10-09 13:59:53 -0500

    update queries.js

---
+++ client/src/components/Results/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_ME, GET_USER_INFO } from '../utils/queries'
+
+
+const Results = () => {
+
+
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Results;
