7258a0f13c62d2d5155acbdef8b584b1ef3a6d48
ThereWolf <bdunham484@gmail.com>
2022-10-09 14:08:42 -0500

    results update

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -5,6 +5,14 @@ import { GET_ME, GET_USER_INFO } from '../utils/queries'
 
 const Results = () => {
 
+    const { loading, data: myData } = useQuery(GET_ME)
+
+    const { data } = useQuery(GET_USER_INFO, {
+        variables: { username: opponent },
+        skip: !answersSubmitted,
+        pollInterval: 1000
+    });
+
 
     return (
         <div>
