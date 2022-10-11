19db3174d7c7eebc1dfdf28f8076b1689e23682b
ThereWolf <bdunham484@gmail.com>
2022-10-11 16:14:31 -0500

    add home link

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -37,8 +37,10 @@ const Header = () => {
   return (
     <header className={classes.header}>
       <div className={classes.header__content}>
-        <h2 className={classes.header__content__logo}><span>N</span>ow<span>Y</span>ou<span>K</span>now</h2>
-
+        <Link to="/">
+          <h2 className={classes.header__content__logo}><span>N</span>ow<span>Y</span>ou<span>K</span>now</h2>
+        </Link>
+        
         <nav
           className={`${classes.header__content__nav} ${
             menuOpen ? classes.isMenu : ""
