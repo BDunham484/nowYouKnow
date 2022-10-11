32cf53af02c0827af7fae06e47aa3c6b2486eae9
Becca Doan <rebecca.doan@yahoo.com>
2022-10-11 14:20:47 -0500

    render results to page

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -12,6 +12,7 @@ const Results = () => {
     const [GameData, setGameData] = useState({username: '', opponent: '', QandA: [], opponentQandA: [], category: ''})
     const { username, opponent, QandA, opponentQandA, category } = GameData
     const [finalGame, setFinalGame] = useState({username: '', opponent: '', yourScore: '', opponentScore: '', winner: '', question: []})
+    const { username: finalUsername, opponent: finalOpponent, yourScore, opponentScore, winner, questionsArray } = finalGame
 
     useEffect(() => {
     if(!loading) {
@@ -43,19 +44,50 @@ const Results = () => {
                 yourScore: finalResults.yourScore,
                 opponentScore: finalResults.opponentScore,
                 winner: finalResults.winner,
-                question: finalResults.question
+                questionsArray: finalResults.question
             })
         }
       }, [GameData])
 
     return (
         <div>
-            {finalGame.username ? (
+            {!finalUsername ? (
                 <p>loading...</p>
             ) : (
                 <>
                     <div>
-
+                        <h2>Your results with {opponent}...
+                        {winner === "It's a tie!!" ? (<span>{winner}</span>) : (
+                        <>
+                            {winner === username ? (
+                                <span>you won</span>
+                            ):(
+                                <span>{winner} won</span>
+                            )}
+                        </>
+                        )}</h2>
+                        <h3>Your final score: {yourScore}. {opponent}'s final score: {opponentScore}</h3>
+                        {questionsArray.map(question => (
+                            <div>
+                                Question: {question.questionBody}
+                                <div>
+                                    <span>
+                                        Your Answer: {question.yourAnswer}
+                                    </span>
+                                    <span>
+                                        Opponent's Guess: {question.opponentQuess}
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        Opponent's Answer: {question.opponentAnswer}
+                                    </span>
+                                    <span>
+                                        Your Guess: {question.yourQuess}
+                                    </span>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </>
             )}
