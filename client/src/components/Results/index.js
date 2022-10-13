398d81902faa24fedf8a43f455eeb97b15acaa06
Neri Maldonado <slachelier@gmail.com>
2022-10-13 18:36:31 -0500

    styles merge

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './Results.scss';
 
 const Results = ({finalGameResults}) => {
     const { username, opponent, yourScore, opponentScore, winner, questionsArray } = finalGameResults
 
     return (
         <div>
-            <div>
+            <div className="results">
                 <h2>Your results with {opponent}...
                 {winner === "It's a tie!!" ? (<span>{winner}</span>) : (
                 <>
@@ -16,24 +17,36 @@ const Results = ({finalGameResults}) => {
                     )}
                 </>
                 )}</h2>
-                <h3>Your final score: {yourScore}. {opponent}'s final score: {opponentScore}</h3>
+                <h3>Your final score: {yourScore}</h3>
+                <h3>{opponent}'s final score: {opponentScore}</h3>
+                <div className="result-titles">
+                    <h3 id="Qs">Questions:</h3>
+                    <h3 id="yours">Your Responses:</h3>
+                    <h3 id="theirs">Their Responses:</h3>
+                </div>
                 {questionsArray.map((question, index) => (
-                    <div key={index}>
-                        Question: {question.questionBody}
-                        <div>
-                            <span>
-                                Your Answer: {question.yourAnswer}
+                    <div className="responses" key={index}>
+                        <span className="questions">
+                            <p>{question.questionBody}</p>
+                        </span>
+                        <div className="their-responses">
+                            <span className="your-ans">
+                                <h4>Your Answer:</h4> 
+                                <p>{question.yourAnswer}</p>
                             </span>
-                            <span>
-                                Opponent's Guess: {question.opponentGuess}
+                            <span className="their-guess">
+                                <h4>Their Guess:</h4> 
+                                <p>{question.opponentGuess}</p>
                             </span>
                         </div>
-                        <div>
-                            <span>
-                                Opponent's Answer: {question.opponentAnswer}
+                        <div className="your-responses">
+                            <span className="their-ans">
+                                <h4>Their Answer: </h4>
+                                <p>{question.opponentAnswer}</p>
                             </span>
-                            <span>
-                                Your Guess: {question.yourGuess}
+                            <span className="your-guess">
+                                <h4>Your Guess: </h4>
+                                <p>{question.yourGuess}</p>
                             </span>
                         </div>
                     </div>
